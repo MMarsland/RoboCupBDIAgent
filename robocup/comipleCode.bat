6cd2bee0cf3991f474e2cd54cf4292207a638bed
James_WE_Horner <jameshorner@cmail.carleton.ca>
2021-12-08 19:40:08 -0500

    Added comments to the BAT files referenced in the report.

--- robocup/comipleCode.bat
+++ robocup/comipleCode.bat
@@ -1,4 +1,10 @@
+@echo off
+
+REM Go to the BDI Agent directory.
 cd BDIAgentKrislet
+
+REM Run the Java compiler on all JAVA files with the Jason JAR file on the class path.
 javac -cp .;jason-2.3.jar *.java
-java -cp .;jason-2.3.jar JasonAgent
+
+REM Return to the original directory.
 cd ..
--- robocup/playBDIvsKrislet.bat
+++ robocup/playBDIvsKrislet.bat
@@ -1,19 +1,26 @@
+REM Start the RoboCup server.
 cd ./Server
 start ./rcssserver.exe
 cd ..
 timeout 1
+
+REM Start the RoboCup monitor.
 cd ./Monitor
 start ./rcssmonitor.exe
 cd ..
 timeout 1
+
+REM Start a team of Jason BDI agents with different roles.
 cd ./BDIAgentKrislet
 start java -cp .;jason-2.3.jar Krislet -team BDI -asl goalie.asl
 start java -cp .;jason-2.3.jar Krislet -team BDI -asl defender.asl
-start java -cp .;jason-2.3.jar Krislet -team BDI -asl centreback.asl
+start java -cp .;jason-2.3.jar Krislet -team BDI -asl defender.asl
 start java -cp .;jason-2.3.jar Krislet -team BDI -asl midfielder.asl
 start java -cp .;jason-2.3.jar Krislet -team BDI -asl attacker.asl
 cd ..
 timeout 1
+
+REM Start a team of default Krislet players.
 cd ./OriginalKrislet
 start java Krislet -team Krislet
 start java Krislet -team Krislet
